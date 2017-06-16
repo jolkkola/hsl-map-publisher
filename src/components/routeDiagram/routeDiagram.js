db59b5adeeb4bf1f41e41ee1981eb47804d831d2
Hannes Junnila <hannes.junnila@gmail.com>
2017-06-16 11:14:33 +0300

    Do not render empty route diagram
    Eg. for last stops with different departure stop

--- src/components/routeDiagram/routeDiagram.js
+++ src/components/routeDiagram/routeDiagram.js
@@ -5,24 +5,26 @@ import Path from "./path";
 import styles from "./routeDiagram.css";
 
 const RouteDiagram = props => (
-    <div className={styles.root}>
-        <div className={styles.componentName}>
-            <div className={styles.title}>
-                Linjojen reitit
+    props.tree.length === 0
+        ? null
+        : <div className={styles.root}>
+            <div className={styles.componentName}>
+                <div className={styles.title}>
+                      Linjojen reitit
+                  </div>
+                <div className={styles.subtitle}>
+                      Linjernas rutter
+                  </div>
             </div>
-            <div className={styles.subtitle}>
-                Linjernas rutter
+            <div className={styles.start}>
+                <img src={markerIcon} className={styles.icon} role="presentation"/>
+                <div className={styles.myPositionTitle}>
+                      Olet tässä&nbsp;&nbsp;
+                      <span className={styles.subtitle}>Du är här</span>
+                </div>
             </div>
+            <Path {...props.tree}/>
         </div>
-        <div className={styles.start}>
-            <img src={markerIcon} className={styles.icon} role="presentation"/>
-            <div className={styles.myPositionTitle}>
-              Olet tässä&nbsp;&nbsp;
-              <span className={styles.subtitle}>Du är här</span>
-            </div>
-        </div>
-        <Path {...props.tree}/>
-    </div>
     );
 
 export default RouteDiagram;
