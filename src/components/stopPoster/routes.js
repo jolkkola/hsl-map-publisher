1b3ce9e6af9c85e02c7f6f44c9b009a15753f50d
Antti Silventoinen <antti.silventoinen@iki.fi>
2017-06-21 18:47:59 +0300

    Show departure intervals for trunk routes

--- src/components/stopPoster/routes.js
+++ src/components/stopPoster/routes.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import chunk from "lodash/chunk";
 import sortBy from "lodash/sortBy";
 import { Row, Column, Image } from "components/util";
 
 import renderQueue from "util/renderQueue";
 import { isTrunkRoute, getColor, getIcon, colorsByMode } from "util/domain";
 
+import timeIcon from "icons/time.svg";
+
 import styles from "./routes.css";
 
 const MAX_COLUMNS = 6;
 
 const Icon = props => <Image {...props} className={styles.icon}/>;
+
+const DepartureIntervals = () => (
+    <div className={styles.interval}>
+        <Row>
+            <Image className={styles.intervalIcon} src={timeIcon}/>
+            <Column>
+                <div className={styles.intervalTitle}>ma-pe päivällä</div>
+                <div className={styles.intervalSubtitle}>må-fre dagtid</div>
+            </Column>
+            <div className={styles.intervalValue}>3-5 min</div>
+
+            <Column>
+                <div className={styles.intervalTitle}>la-su päivisin</div>
+                <div className={styles.intervalSubtitle}>lö-sö dagtid</div>
+            </Column>
+            <div className={styles.intervalValue}>8-10 min</div>
+        </Row>
+    </div>
+);
 
 function replaceColor(src) {
     return `data:image/svg+xml;base64,${btoa(atob(src.replace("data:image/svg+xml;base64,", "")).replace("#D5E8FA", "#FFFFFF"))}`;
@@ -58,15 +78,29 @@ class Routes extends Component {
 
     render() {
         if (this.props.routes.length === 0) {
-            console.error("No routes for stop");
+            console.error("No routes for stop"); // eslint-disable-line no-console
             renderQueue.remove(this, { success: false });
         }
 
+        const sortedRoutes = sortBy(this.props.routes, route => !isTrunkRoute(route.routeId));
         const routesPerColumn = Math.ceil(this.props.routes.length / this.state.columns);
-        const routeColumns = chunk(
-            sortBy(this.props.routes, route => !isTrunkRoute(route.routeId)),
-            routesPerColumn
-        );
+
+        let routeCount = 0;
+        let columnIndex = 0;
+        const routeColumns = [];
+        // Split routes into groups
+        sortedRoutes.forEach((route) => {
+            if (routeCount >= routesPerColumn) {
+                columnIndex++;
+                routeCount = 0;
+            }
+            if (!routeColumns[columnIndex]) {
+                routeColumns[columnIndex] = [];
+            }
+            routeColumns[columnIndex].push(route);
+            // Trunk routes take up twice as much space
+            routeCount += (isTrunkRoute(route.routeId) ? 2 : 1);
+        });
 
         if (this.props.routes.some(route => isTrunkRoute(route.routeId))) {
             // TODO: This is a hack to set the background color for stops with trunk routes
@@ -82,42 +116,66 @@ class Routes extends Component {
 
         return (
             <div className={styles.root} ref={(ref) => { this.root = ref; }}>
-                {routeColumns.map((routes, i) => (
-                    <Row key={i}>
-                        <Column>
-                            {routes.map((route, index) => (
-                                <div key={index} className={styles.group}>
-                                    <Icon src={getIcon(route)}/>
-                                </div>
-                            ))}
-                        </Column>
-                        <Column>
-                            {routes.map((route, index) => (
-                                <div key={index} className={styles.group}>
-                                    <div className={styles.id} style={{ color: getColor(route) }}>
-                                        {route.routeId}
+                {routeColumns.map((routes, i) => {
+                    const hasTrunkRoutes = routes.some(({ routeId }) => isTrunkRoute(routeId));
+
+                    return (
+                        <Row key={i} style={hasTrunkRoutes ? { width: 500 } : null}>
+                            <Column>
+                                {routes.map((route, index) => (
+                                    <div>
+                                        <div key={index} className={styles.group}>
+                                            <Icon src={getIcon(route)}/>
+                                        </div>
+                                        {isTrunkRoute(route.routeId) &&
+                                            <div className={styles.group}>
+                                                <DepartureIntervals/>
+                                            </div>
+                                        }
                                     </div>
-                                </div>
-                            ))}
-                        </Column>
-                        <Column>
-                            {routes.map((route, index) => (
-                                <div
-                                    key={index}
-                                    className={styles.group}
-                                    style={{ color: getColor(route) }}
-                                >
-                                    <div className={styles.title}>
-                                        {route.destinationFi}
+                                ))}
+                            </Column>
+                            <Column>
+                                {routes.map((route, index) => (
+                                    <div key={index}>
+                                        <div className={styles.group}>
+                                            <div
+                                                className={styles.id}
+                                                style={{ color: getColor(route) }}
+                                            >
+                                                {route.routeId}
+                                            </div>
+                                        </div>
+                                        {isTrunkRoute(route.routeId) &&
+                                            <div className={styles.group}/>
+                                        }
                                     </div>
-                                    <div className={styles.subtitle}>
-                                        {route.destinationSe}
+                                ))}
+                            </Column>
+                            <Column>
+                                {routes.map((route, index) => (
+                                    <div>
+                                        <div
+                                            key={index}
+                                            className={styles.group}
+                                            style={{ color: getColor(route) }}
+                                        >
+                                            <div className={styles.title}>
+                                                {route.destinationFi}
+                                            </div>
+                                            <div className={styles.subtitle}>
+                                                {route.destinationSe}
+                                            </div>
+                                        </div>
+                                        {isTrunkRoute(route.routeId) &&
+                                            <div className={styles.group}/>
+                                        }
                                     </div>
-                                </div>
-                            ))}
-                        </Column>
-                    </Row>
-                ))}
+                                ))}
+                            </Column>
+                        </Row>
+                    );
+                })}
             </div>
         );
     }
